ba4357e331e510fae904828b38325ec5868c3db1
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2023-08-17 22:42:16 +0700

    4. Xây dựng UI phần Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,103 @@
 import classNames from 'classnames/bind';
+import {
+    Bookmark,
+    Chat,
+    Event,
+    Group,
+    HelpOutline,
+    PlayCircleFilledOutlined,
+    RssFeed,
+    School,
+    WorkOutline,
+} from '@mui/icons-material';
+
 import styles from './Sidebar.module.scss';
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
-    return <div className={cx('wrapper')}>Sidebar</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('container')}>
+                <ul className={cx('list')}>
+                    <li className={cx('list-item')}>
+                        <RssFeed className={cx('icon')} />
+                        <span className={cx('list-item-text')}>Feed</span>
+                    </li>
+                    <li className={cx('list-item')}>
+                        <Chat className={cx('icon')} />
+                        <span className={cx('list-item-text')}>Chats</span>
+                    </li>
+                    <li className={cx('list-item')}>
+                        <PlayCircleFilledOutlined className={cx('icon')} />
+                        <span className={cx('list-item-text')}>Videos</span>
+                    </li>
+                    <li className={cx('list-item')}>
+                        <Group className={cx('icon')} />
+                        <span className={cx('list-item-text')}>Groups</span>
+                    </li>
+                    <li className={cx('list-item')}>
+                        <Bookmark className={cx('icon')} />
+                        <span className={cx('list-item-text')}>Bookmarks</span>
+                    </li>
+                    <li className={cx('list-item')}>
+                        <HelpOutline className={cx('icon')} />
+                        <span className={cx('list-item-text')}>Questions</span>
+                    </li>
+                    <li className={cx('list-item')}>
+                        <WorkOutline className={cx('icon')} />
+                        <span className={cx('list-item-text')}>Jobs</span>
+                    </li>
+                    <li className={cx('list-item')}>
+                        <Event className={cx('icon')} />
+                        <span className={cx('list-item-text')}>Events</span>
+                    </li>
+                    <li className={cx('list-item')}>
+                        <School className={cx('icon')} />
+                        <span className={cx('list-item-text')}>Courses</span>
+                    </li>
+                </ul>
+
+                <button className={cx('btn')}>Show More</button>
+                <hr className={cx('hr')} />
+
+                <ul className={cx('friend-list')}>
+                    <li className={cx('friend')}>
+                        <img src="/assets/person/2.jpeg" alt="" className={cx('img')} />
+                        <span className={cx('friend-name')}>Jane Doe</span>
+                    </li>
+                    <li className={cx('friend')}>
+                        <img src="/assets/person/2.jpeg" alt="" className={cx('img')} />
+                        <span className={cx('friend-name')}>Jane Doe</span>
+                    </li>
+                    <li className={cx('friend')}>
+                        <img src="/assets/person/2.jpeg" alt="" className={cx('img')} />
+                        <span className={cx('friend-name')}>Jane Doe</span>
+                    </li>
+                    <li className={cx('friend')}>
+                        <img src="/assets/person/2.jpeg" alt="" className={cx('img')} />
+                        <span className={cx('friend-name')}>Jane Doe</span>
+                    </li>
+                    <li className={cx('friend')}>
+                        <img src="/assets/person/2.jpeg" alt="" className={cx('img')} />
+                        <span className={cx('friend-name')}>Jane Doe</span>
+                    </li>
+                    <li className={cx('friend')}>
+                        <img src="/assets/person/2.jpeg" alt="" className={cx('img')} />
+                        <span className={cx('friend-name')}>Jane Doe</span>
+                    </li>
+                    <li className={cx('friend')}>
+                        <img src="/assets/person/2.jpeg" alt="" className={cx('img')} />
+                        <span className={cx('friend-name')}>Jane Doe</span>
+                    </li>
+                    <li className={cx('friend')}>
+                        <img src="/assets/person/2.jpeg" alt="" className={cx('img')} />
+                        <span className={cx('friend-name')}>Jane Doe</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Sidebar;
